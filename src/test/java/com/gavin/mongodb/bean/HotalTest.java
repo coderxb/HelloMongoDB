8d1155bf434139716c63e08b5486d1e14d8df643
xuebing.li <coderxb@163.com>
2016-12-07 15:47:39 +0800

    fix code

--- src/test/java/com/gavin/mongodb/bean/HotalTest.java
+++ src/test/java/com/gavin/mongodb/bean/HotalTest.java
@@ -1,11 +1,8 @@
 package com.gavin.mongodb.bean;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.gavin.mongodb.common.MorphiaBean;
 import com.github.jmkgreen.morphia.Datastore;
 
